50de46fd5c5bfe59a43ab3c69900fa8c9c8ab7f0
deepakdey412@gmail.com <deepakdey412@gmail.com>
2025-10-23 23:21:32 +0530

    add controller

---
+++ user-management-system/user-management-system/src/main/java/com/ddey/user_management_system/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.ddey.user_management_system.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/user-api" , produces = MediaType.APPLICATION_JSON_VALUE)
+public class UserController {
+
+}
