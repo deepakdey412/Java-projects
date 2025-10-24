8ea1d578e9ef9c0fc85e165612b4003c34a598df
deepakdey412@gmail.com <deepakdey412@gmail.com>
2025-10-24 08:58:48 +0530

    Add Model mapper configuration class

---
+++ user-management-system/user-management-system/src/main/java/com/ddey/user_management_system/mapper/ModelMapperConfig.java
@@ -0,0 +1,15 @@
+package com.ddey.user_management_system.mapper;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper;
+    }
+}
